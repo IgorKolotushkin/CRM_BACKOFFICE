23aeff6401dedb204f98a783083a31991cec1c86
igor <dr_kia@mail.ru>
2023-12-29 10:10:59 +0300

    start project

---
+++ crm_backoffice/crm_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrmAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'crm_auth'
---
+++ crm_backoffice/crm_auth/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = "crm_auth"
+
+urlpatterns = [
+    # path("", shop_index, name="index"),
+]
