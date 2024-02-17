c5836fc55acba7b6008ca29b7aa9a24e5752e28c
igor <dr_kia@mail.ru>
2024-02-17 18:07:02 +0300

    added part tests

---
+++ crm_backoffice/conftest.py
@@ -0,0 +1,47 @@
+import pytest
+from django.db import connections
+from django.test import Client
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
+from crm_auth.models import User
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+def run_sql(sql):
+    conn = psycopg2.connect(
+        database='crm_db',
+        # dbname='the_copied_db',
+        user='postgres',
+        password='postgres',
+        host='127.0.0.1',
+        port=5432
+    )
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.close()
+
+
+@pytest.fixture(scope='session', autouse=True)
+def django_db_setup():
+    from django.conf import settings
+
+    settings.DATABASES['default']['NAME'] = 'test_db'
+    settings.DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
+    settings.DATABASES['default']['USER'] = 'postgres'
+    settings.DATABASES['default']['PASSWORD'] = 'postgres'
+    settings.DATABASES['default']['HOST'] = '127.0.0.1'
+    settings.DATABASES['default']['PORT'] = 5432
+
+    run_sql('DROP DATABASE IF EXISTS test_db')
+    run_sql('CREATE DATABASE test_db TEMPLATE crm_db')
+
+
+    yield
+
+    for connection in connections.all():
+        connection.close()
+
+    run_sql('DROP DATABASE test_db')
--- crm_backoffice/office/tests.py
+++ crm_backoffice/office/tests.py
@@ -1,3 +1,81 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.urls import reverse
+from django.test import Client
+from django.contrib.auth.models import User, Group, Permission
+from django.views.decorators.csrf import csrf_exempt
+import pytest
+
+from .models import Product, Lead, Ads, Contract, Customer
+
+csrf_client = Client(enforce_csrf_checks=True)
+
+
+def test_login_user():
+    data = {
+        'name': 'igor',
+        'password': '123',
+    }
+    url = reverse('crm_auth:login')
+    response = csrf_client.post(url, data)
+
+
+def test_create_product(db):
+    count_product = len(Product.objects.all())
+    product = Product.objects.create(name="product", cost=1000)
+    assert product.name == "product" and product.cost == 1000
+
+    count_product_new = len(Product.objects.all())
+    assert count_product + 1 == count_product_new
+
+
+# @pytest.mark.django_db
+def test_view_login_inactive_user(client):
+    url = reverse('office:index')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == '/accounts/login/?next=/stat/'
+
+
+# @pytest.mark.django_db
+def test_superuser_view_leads_list(admin_client):
+    lead = Lead.objects.first()
+    url = reverse('office:leads')
+    response = admin_client.get(url)
+    assert response.status_code == 200
+    assert "leads/leads-list.html" in response.template_name
+    assert response.context_data['leads'][0].first_name == lead.first_name
+    assert response.context_data['leads'][0].email == lead.email
+
+
+def test_superuser_add_lead_db(admin_client):
+    count_leads = len(Lead.objects.all())
+    lead = Lead.objects.create(
+        first_name='first_test',
+        last_name='last_test',
+        email='email@email.com',
+        phone='89001111111'
+    )
+    assert lead.first_name == 'first_test'
+    assert lead.email == 'email@email.com'
+    assert len(Lead.objects.all()) == count_leads + 1
+
+
+def test_superuser_create_lead(admin_client):
+    csrf_client.login(username="igor", password="123")
+
+    data = {
+        'first_name': 'first_test_2',
+        'last_name': 'last_test_2',
+        'email': 'email@email_1.com',
+        'phone': '89001111111',
+        'ads': 'ads',
+    }
+    count_lead = len(Lead.objects.all())
+    url = reverse('office:create-lead')
+    response = admin_client.post(url, data)
+    # response = csrf_client.post(url, fields=data)
+    assert response.status_code == 200
+    # assert response.context['form'] == ''
+    assert count_lead + 1 == len(Lead.objects.all())
+
